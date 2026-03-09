178f952badc4510b94110b6822bdc8d2b7e9e6c1
Govind <GOVINDAMBADE28@GMAIL.COM>
2026-03-09 21:24:25 +0530

    Implement HTTP server that returns random color

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,22 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import random
+import json
 
+colors = ["red","blue","green","yellow","purple","orange"]
+
+class handler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        color = random.choice(colors)
+
+        self.send_response(200)
+        self.send_header("Content-type","application/json")
+        self.end_headers()
+
+        response = {"color": color}
+        self.wfile.write(json.dumps(response).encode())
+
+server = HTTPServer(("0.0.0.0",5000), handler)
+
+print("Backend running on port 5000")
+server.serve_forever()
